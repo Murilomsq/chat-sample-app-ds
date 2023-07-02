afbedbcef3abf6ffb14b349c3468b0f9e41ec243
Murilo <56661791+Murilomsq@users.noreply.github.com>
2023-07-02 19:20:59 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
 CHAT_SERVER_HOST = "10.128.0.3"
-CHAT_SERVER_PORT = 5001
+CHAT_SERVER_PORT = 5678
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
 registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
