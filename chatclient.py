6d068817c205f20e98710c6f15f02800e399f3b2
Murilo <56661791+Murilomsq@users.noreply.github.com>
2023-07-02 19:58:56 -0300

    Update chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -18,33 +18,27 @@ class RecvHandler(threading.Thread):
             conn.send(pickle.dumps("ACK"))
             conn.close()
 
-class SendHandler(threading.Thread):
-    def __init__(self, sock):
-        threading.Thread.__init__(self)
-        self.server_socket = sock
+def send_message():
+    dest = input("ENTER DESTINATION: ")
+    msg = input("ENTER MESSAGE: ")
 
-    def run(self):
-        while True:
-            dest = input("ENTER DESTINATION: ")
-            msg = input("ENTER MESSAGE: ")
+    try:
+        server_sock = socket(AF_INET, SOCK_STREAM)
+        server_sock.connect((const.CHAT_SERVER_HOST, const.CHAT_SERVER_PORT))
+    except:
+        print("Server is down. Exiting...")
+        exit(1)
 
-            try:
-                server_sock = socket(AF_INET, SOCK_STREAM)
-                server_sock.connect((const.CHAT_SERVER_HOST, const.CHAT_SERVER_PORT))
-            except:
-                print("Server is down. Exiting...")
-                exit(1)
-
-            msg_pack = (msg, dest, me)
-            marshaled_msg_pack = pickle.dumps(msg_pack)
-            server_sock.send(marshaled_msg_pack)
-            marshaled_reply = server_sock.recv(1024)
-            reply = pickle.loads(marshaled_reply)
-            if reply != "ACK":
-                print("Error: Server did not accept the message (dest does not exist?)")
-            else:
-                pass
-            server_sock.close()
+    msg_pack = (msg, dest, me)
+    marshaled_msg_pack = pickle.dumps(msg_pack)
+    server_sock.send(marshaled_msg_pack)
+    marshaled_reply = server_sock.recv(1024)
+    reply = pickle.loads(marshaled_reply)
+    if reply != "ACK":
+        print("Error: Server did not accept the message (dest does not exist?)")
+    else:
+        pass
+    server_sock.close()
 
 try:
     me = str(sys.argv[1])
@@ -60,5 +54,5 @@ client_sock.listen(5)
 recv_handler = RecvHandler(client_sock)
 recv_handler.start()
 
-send_handler = SendHandler(client_sock)
-send_handler.start()
+while True:
+    send_message()
